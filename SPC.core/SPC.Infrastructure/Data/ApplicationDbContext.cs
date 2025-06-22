0347e58f615ed54a96e9c0ad156374869cec8d67
Tim De Ruyck <113930948+TimDeRuyck@users.noreply.github.com>
2025-06-22 16:46:06 +0200

    Add ApplicationDbContext
    Removed folder reference from SPC.Infrastructure.csproj and retained project reference to SPC.core.csproj. Added ApplicationDbContext class in a new file, which includes DbSet properties for MeasurementType, RawMeasurement, Site, and UserMeasurementType, facilitating entity management within the application context.

---
+++ SPC.core/SPC.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SPC.core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPC.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+        public DbSet<MeasurementType> MeasurementTypes { get; set; }
+        public DbSet<RawMeasurement> RawMeasurements { get; set; }
+        public DbSet<Site> Sites { get; set; }
+        public DbSet<UserMeasurementType> UserMeasurementTypes { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
