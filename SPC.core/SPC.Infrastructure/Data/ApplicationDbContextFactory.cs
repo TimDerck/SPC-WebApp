fb43d2bb5167354ac290b94dd10b23256f9163c7
Tim De Ruyck <113930948+TimDeRuyck@users.noreply.github.com>
2025-06-22 17:56:08 +0200

    Add configuration packages and DbContext factory
    Updated `SPC.Infrastructure.csproj` to include Microsoft.Extensions.Configuration packages (v9.0.6) for enhanced configuration management.
    
    Added `ApplicationDbContextFactory` class to facilitate design-time creation of `ApplicationDbContext` instances, enabling Entity Framework Core to connect to the database using connection strings from configuration files.

---
+++ SPC.core/SPC.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO; // Add this using directive
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPC.Infrastructure.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) 
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
