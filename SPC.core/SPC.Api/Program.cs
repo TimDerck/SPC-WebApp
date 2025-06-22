d384b90be18301a6d1271a3f0f8485c933755e22
Tim De Ruyck <113930948+TimDeRuyck@users.noreply.github.com>
2025-06-22 17:50:41 +0200

    Add Identity and EF Core setup in Program.cs
    Updated Program.cs to configure ASP.NET Core Identity and Entity Framework Core with SQL Server. Added necessary middleware for authentication and authorization, and set up Swagger for API documentation in development.
    
    Modified SPC.Api.csproj to include new package references for Identity and EF Core, and added project references to SPC.core and SPC.Infrastructure.

--- SPC.core/SPC.Api/Program.cs
+++ SPC.core/SPC.Api/Program.cs
@@ -1,4 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SPC.core.Entities;
+using SPC.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+//database context
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//ASP.NET Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 // Add services to the container.
 
@@ -18,6 +33,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
